403d12b254ece0518a531e35a799b59e13f92f3b
Daisyqk <46420960+Daisyqk@users.noreply.github.com>
2022-04-04 23:55:35 +0800

    Delete asr directory

--- asr/espnet_local/utils/print.py
+++
@@ -1,8 +0,0 @@
-import time
-import torch.distributed as dist
-
-
-def step_print(ctx, flush=False):
-    tmark = time.asctime(time.localtime(time.time()))
-    rank = dist.get_rank()
-    print(f"{tmark} | rank: {rank} | {ctx}", flush=flush)
--- asr/start.sh
+++
@@ -1,3 +0,0 @@
-log_path='asr.log'
-python -u ./train.py > $log_path 2>&1
-
